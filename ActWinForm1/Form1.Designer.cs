99afdd0673d854a3c416a8f6a2c96df97750ed21
Stefan <stefan.pimentel.college@ustangelicum.edu.ph>
2025-11-30 01:50:59 +0800

    Update Form1 layout and add title label

--- ActWinForm1/Form1.Designer.cs
+++ ActWinForm1/Form1.Designer.cs
@@ -35,22 +35,25 @@
             lstTasks = new ListBox();
             lblStatus = new Label();
             chkConfirmDelete = new CheckBox();
+            label1 = new Label();
             SuspendLayout();
             // 
             // txtTask
             // 
-            txtTask.Location = new Point(274, 87);
+            txtTask.Location = new Point(391, 145);
+            txtTask.Margin = new Padding(4, 5, 4, 5);
             txtTask.Name = "txtTask";
-            txtTask.Size = new Size(300, 23);
+            txtTask.Size = new Size(427, 31);
             txtTask.TabIndex = 0;
             // 
             // btnAddTask
             // 
             btnAddTask.BackColor = SystemColors.MenuHighlight;
             btnAddTask.ForeColor = Color.FloralWhite;
-            btnAddTask.Location = new Point(499, 127);
+            btnAddTask.Location = new Point(713, 212);
+            btnAddTask.Margin = new Padding(4, 5, 4, 5);
             btnAddTask.Name = "btnAddTask";
-            btnAddTask.Size = new Size(75, 23);
+            btnAddTask.Size = new Size(107, 38);
             btnAddTask.TabIndex = 1;
             btnAddTask.Text = "Add Task";
             btnAddTask.UseVisualStyleBackColor = false;
@@ -60,9 +63,10 @@
             // 
             btnRemoveSelected.BackColor = SystemColors.MenuHighlight;
             btnRemoveSelected.ForeColor = Color.WhiteSmoke;
-            btnRemoveSelected.Location = new Point(274, 127);
+            btnRemoveSelected.Location = new Point(391, 212);
+            btnRemoveSelected.Margin = new Padding(4, 5, 4, 5);
             btnRemoveSelected.Name = "btnRemoveSelected";
-            btnRemoveSelected.Size = new Size(75, 23);
+            btnRemoveSelected.Size = new Size(107, 38);
             btnRemoveSelected.TabIndex = 2;
             btnRemoveSelected.Text = "Remove";
             btnRemoveSelected.UseVisualStyleBackColor = false;
@@ -72,9 +76,10 @@
             // 
             btnClearAll.BackColor = SystemColors.MenuHighlight;
             btnClearAll.ForeColor = Color.WhiteSmoke;
-            btnClearAll.Location = new Point(389, 127);
+            btnClearAll.Location = new Point(556, 212);
+            btnClearAll.Margin = new Padding(4, 5, 4, 5);
             btnClearAll.Name = "btnClearAll";
-            btnClearAll.Size = new Size(75, 23);
+            btnClearAll.Size = new Size(107, 38);
             btnClearAll.TabIndex = 3;
             btnClearAll.Text = "Clear All";
             btnClearAll.UseVisualStyleBackColor = false;
@@ -83,36 +88,50 @@
             // lstTasks
             // 
             lstTasks.FormattingEnabled = true;
-            lstTasks.ItemHeight = 15;
-            lstTasks.Location = new Point(371, 203);
+            lstTasks.ItemHeight = 25;
+            lstTasks.Location = new Point(530, 338);
+            lstTasks.Margin = new Padding(4, 5, 4, 5);
             lstTasks.Name = "lstTasks";
-            lstTasks.Size = new Size(120, 94);
+            lstTasks.Size = new Size(170, 154);
             lstTasks.TabIndex = 4;
             // 
             // lblStatus
             // 
             lblStatus.AutoSize = true;
-            lblStatus.Location = new Point(412, 300);
+            lblStatus.Location = new Point(580, 516);
+            lblStatus.Margin = new Padding(4, 0, 4, 0);
             lblStatus.Name = "lblStatus";
-            lblStatus.Size = new Size(39, 15);
+            lblStatus.Size = new Size(60, 25);
             lblStatus.TabIndex = 5;
             lblStatus.Text = "Status";
             // 
             // chkConfirmDelete
             // 
             chkConfirmDelete.AutoSize = true;
-            chkConfirmDelete.Location = new Point(371, 178);
+            chkConfirmDelete.Location = new Point(530, 297);
+            chkConfirmDelete.Margin = new Padding(4, 5, 4, 5);
             chkConfirmDelete.Name = "chkConfirmDelete";
-            chkConfirmDelete.Size = new Size(106, 19);
+            chkConfirmDelete.Size = new Size(157, 29);
             chkConfirmDelete.TabIndex = 6;
             chkConfirmDelete.Text = "Confirm Delete";
             chkConfirmDelete.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(509, 62);
+            label1.Name = "label1";
+            label1.Size = new Size(205, 25);
+            label1.TabIndex = 7;
+            label1.Text = "Task Management Menu";
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = SystemColors.Info;
+            ClientSize = new Size(1143, 750);
+            Controls.Add(label1);
             Controls.Add(chkConfirmDelete);
             Controls.Add(lblStatus);
             Controls.Add(lstTasks);
@@ -120,8 +139,9 @@
             Controls.Add(btnRemoveSelected);
             Controls.Add(btnAddTask);
             Controls.Add(txtTask);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Task Manager";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -135,5 +155,6 @@
         private ListBox lstTasks;
         private Label lblStatus;
         private CheckBox chkConfirmDelete;
+        private Label label1;
     }
 }
